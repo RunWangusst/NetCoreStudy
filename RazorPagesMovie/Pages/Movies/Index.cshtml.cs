b7360a6e6d1456a4cd073f3bd23867a1aa41231e
wr <1291223834@qq.com>
2023-02-19 11:29:16 +0800

    1. initial files;

---
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList?  Geners { get; set; }
+        [BindProperty(SupportsGet =true)]
+        public string MovieGener { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                IQueryable<string> generQuery = from m in _context.Movie
+                                                orderby m.Genre
+                                                select m.Genre;
+
+
+                var movies = from m in _context.Movie
+                             select m;
+
+                if (!string.IsNullOrWhiteSpace(SearchString))
+                {
+                    movies = movies.Where(s => s.Title.Contains(SearchString));
+                }
+                Movie = await movies.ToListAsync();
+            }
+        }
+    }
+}
